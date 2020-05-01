5ed7af4d17d44f945ac65a3dca31af0593f8cd67
Ed <ed36cuir@hotmail.com>
2020-04-30 21:33:04 -0400

    Changed ports in app.py

--- app.py
+++ app.py
@@ -68,4 +68,4 @@ def predict():
 if __name__ == "__main__":
     # load pretrained model as clf
     clf = joblib.load("./model_data/boston_housing_prediction.joblib")
-    app.run(host='0.0.0.0', port=1025, debug=True) # specify port=1025
+    app.run(host='0.0.0.0', port=80, debug=True) # specify port=1025
