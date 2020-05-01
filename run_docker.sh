fc42635483dd912ae2755e1bd652e39cbc7e44e9
Ed <ed36cuir@hotmail.com>
2020-04-30 20:50:21 -0400

    docker running script

--- run_docker.sh
+++ run_docker.sh
@@ -4,9 +4,12 @@
 
 # Step 1:
 # Build image and add a descriptive tag
+docker build --tag predict-data:0.1 .
 
 # Step 2:
 # List docker images
+docker image ls
 
 # Step 3:
 # Run flask app
+docker run -it --rm --name predict-data predict-data:0.1
