357a8a3b6bb4fca75b0c1006dfe73ebba62d6559
Ed <ed36cuir@hotmail.com>
2020-05-01 19:13:51 -0400

    Upload dockerfile to a repo

--- upload_docker.sh
+++ upload_docker.sh
@@ -5,11 +5,13 @@
 
 # Step 1:
 # Create dockerpath
-# dockerpath=<your docker ID/path>
+dockerpath=ed36cuir/predict-data:0.1
 
 # Step 2:
 # Authenticate & tag
+docker tag 0257b284b3d5 $dockerpath
 echo "Docker ID and Image: $dockerpath"
 
 # Step 3:
 # Push image to a docker repository
+docker push ed36cuir/predict-data:0.1
