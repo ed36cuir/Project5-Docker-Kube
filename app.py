5eb2a92c8a93793017e076eb7ee0b2098b7e59aa
Ed <ed36cuir@hotmail.com>
2020-05-04 13:41:20 -0400

    Added log 'prediction' statement in logs for output_txt_files/docker_out.txt

--- app.py
+++ app.py
@@ -63,6 +63,7 @@ def predict():
     # get an output prediction from the pretrained model, clf
     prediction = list(clf.predict(scaled_payload))
     # TO DO:  Log the output prediction value
+    LOG.info("prediction: \n%s", prediction[0])
     return jsonify({'prediction': prediction})
 
 if __name__ == "__main__":
